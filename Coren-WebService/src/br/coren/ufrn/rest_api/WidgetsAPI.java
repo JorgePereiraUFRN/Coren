ef6b962d7cee2a466bc5853eaca8144f804874e4
Jorge <Jorge@Jorge-PC>
2016-04-01 15:07:02 -0300

    commit

--- Coren-WebService/src/br/coren/ufrn/rest_api/WidgetsAPI.java
+++ Coren-WebService/src/br/coren/ufrn/rest_api/WidgetsAPI.java
@@ -1,5 +1,16 @@
 package br.coren.ufrn.rest_api;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+
 public class WidgetsAPI {
+	
+	
+	@POST
+	@Path("create_widget")
+	public void createWidget(String xml_wdget){
+		
+	}
 
 }
